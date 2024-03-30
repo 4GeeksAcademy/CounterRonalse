d94724e0dc76ebaae76415c6f1613f1c19909d7a
ronalse <157391307+ronalse@users.noreply.github.com>
2024-03-30 18:33:56 +0000

    ronald

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,25 +1,31 @@
-import React from "react";
-import Stop from "/workspaces/CounterRonalse/src/js/component/Stop.jsx";
-import Restart from "/workspaces/CounterRonalse/src/js/component/Restart.jsx";
+import React, { useState, useEffect } from "react";
+
 
 //include images into your bundle
 
-
-
 //create your first component
-const Home = (props) => {
+const Home = () => {
+	let [Counter, setCounter] = useState(0);
+
+  const day = Math.floor(Counter / (60 * 60 * 24));
+  const hour = Math.floor((Counter / (60 * 60)) % 24);
+  const minute = Math.floor((Counter / 60) % 60);
+  const seconds = Counter % 60;
+
+useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [Counter]);
 	return (
 <>
 		<div className="container-fluid d-flex justify-content-center align-items-center mt-5 bg-dark text-white fs-1 m-0">
 				<div className="mx-3 far fa-clock "></div>
-				<div className=" mx-3">{props.Primero % 10}</div>
-				<div className="mx-3">{props.Segundo % 10}</div>
-				<div className="mx-3">{props.Tercero % 10}</div>
-				<div className="mx-3">{props.Cuarto % 10}</div>
-		</div>		
-		<div className="container-fluid  d-flex justify-content-center mt-5">		
-
-
+				<div className=" mx-3 ">{day}</div>
+				<div className="mx-3 ">{hour}</div>
+				<div className="mx-3 ">{minute}</div>
+				<div className="mx-3 ">{seconds}</div>
 		</div>
 </>
 	);
